c848ac8d9c7f0e1f0076d8e22e312f2b9d085d83
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 09:30:31 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        ServerSocket mioServerSocket = new ServerSocket(3000);
+        Socket mioSocket = mioServerSocket.accept();
+        System.out.println("Qualcuno si e' collegato");
+        
     }
 }
